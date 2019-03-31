c4fbaa644b59855a59b2bac006569d2e5b659538
Michał Terczyński <mterczynski271@gmail.com>
2019-03-31 21:35:35 +0200

    Added tslint, fixed tslint errors

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,20 +6,20 @@ export class App {
   private readonly camera = new PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 10000);
   private readonly renderer = new WebGLRenderer({
     antialias: true,
-    canvas: <HTMLCanvasElement>document.getElementById("main-canvas")
+    canvas: document.getElementById('main-canvas') as HTMLCanvasElement,
   });
 
   private brick: Brick;
 
   constructor() {
-    this.brick = new Brick(100, new Color("rgb(0,0,0)"));
+    this.brick = new Brick(100, new Color('rgb(0,0,0)'));
     this.scene.add(this.brick);
 
     this.camera.position.set(200, 200, 200);
-    this.camera.lookAt(<Vector3>{ x: 0, y: 0, z: 0 });
+    this.camera.lookAt({ x: 0, y: 0, z: 0 } as Vector3);
 
     this.renderer.setSize(innerWidth, innerHeight);
-    this.renderer.setClearColor(new Color(0xffffff));
+    this.renderer.setClearColor(new Color('rgb(0,0,0)'));
 
     this.render();
   }
@@ -32,7 +32,7 @@ export class App {
 
   private render() {
     this.renderer.render(this.scene, this.camera);
-    requestAnimationFrame(() => { this.render() });
+    requestAnimationFrame(() => { this.render(); });
 
     this.adjustCanvasSize();
     this.brick.rotateY(0.03);
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
-import { App } from "./app/app";
+import { App } from './app/app';
 
 new App();
