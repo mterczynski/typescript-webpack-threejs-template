dbd30e1c65dcfec2d3da39316da10fec4900bf98
Michał Terczyński <mterczynski271@gmail.com>
2019-03-31 21:24:14 +0200

    code styling

--- src/app/Brick.ts
+++ src/app/Brick.ts
@@ -1,9 +1,9 @@
-import * as THREE from 'three';
+import { BoxGeometry, Color, Mesh, MeshBasicMaterial } from 'three';
 
-export class Brick extends THREE.Mesh {
-  constructor(size: number, color: THREE.Color) {
+export class Brick extends Mesh {
+  constructor(size: number, color: Color) {
     super();
-    this.geometry = new THREE.BoxGeometry(size, size, size);
-    this.material = new THREE.MeshBasicMaterial({ color });
+    this.geometry = new BoxGeometry(size, size, size);
+    this.material = new MeshBasicMaterial({ color });
   }
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,36 +1,40 @@
-import * as THREE from 'three';
+import { Color, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three';
 import { Brick } from './Brick';
 
 export class App {
+  private readonly scene = new Scene();
+  private readonly camera = new PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 10000);
+  private readonly renderer = new WebGLRenderer({
+    antialias: true,
+    canvas: <HTMLCanvasElement>document.getElementById("main-canvas")
+  });
+
+  private brick: Brick;
+
   constructor() {
-    this.brick = new Brick(100, new THREE.Color("rgb(0,0,255)"));
+    this.brick = new Brick(100, new Color("rgb(0,0,0)"));
     this.scene.add(this.brick);
 
     this.camera.position.set(200, 200, 200);
+    this.camera.lookAt(<Vector3>{ x: 0, y: 0, z: 0 });
+
     this.renderer.setSize(innerWidth, innerHeight);
-    this.renderer.setClearColor(new THREE.Color("rgb(0,0,0)"));
-    this.camera.lookAt(<THREE.Vector3>{ x: 0, y: 0, z: 0 });
+    this.renderer.setClearColor(new Color(0xffffff));
 
     this.render();
   }
-
-  private readonly renderer = new THREE.WebGLRenderer({ antialias: true, canvas: <HTMLCanvasElement>document.getElementById("mainCanvas") });
-  private readonly scene = new THREE.Scene();
-  private readonly camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 10000);
-
-  // Example mesh
-  private brick: Brick;
 
   private adjustCanvasSize() {
     this.renderer.setSize(innerWidth, innerHeight);
     this.camera.aspect = innerWidth / innerHeight;
     this.camera.updateProjectionMatrix();
   }
+
   private render() {
     this.renderer.render(this.scene, this.camera);
     requestAnimationFrame(() => { this.render() });
-    this.adjustCanvasSize();
 
+    this.adjustCanvasSize();
     this.brick.rotateY(0.03);
   }
 }
