558fe1b3cc1f796b7174d68a95e367781faf5e36
Michał Terczyński <mterczynski271@gmail.com>
2019-05-07 23:05:20 +0200

    Removed empty comment in Brick.ts

--- src/app/Brick.ts
+++ src/app/Brick.ts
@@ -2,7 +2,7 @@ import { BoxGeometry, Color, Mesh, MeshBasicMaterial } from 'three';
 
 export class Brick extends Mesh {
   constructor(size: number, color: Color) {
-    super(); //
+    super();
     this.geometry = new BoxGeometry(size, size, size);
     this.material = new MeshBasicMaterial({ color });
   }
