6183e5a7aad326425e22b644662afc7b34d903d0
Michal Terczynski <mterczynski271@gmail.com>
2019-12-11 21:00:50 +0100

    Updated publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist'
+    publicPath: '/dist/'
   },
   resolve: {
     extensions: ['.ts', '.js']
